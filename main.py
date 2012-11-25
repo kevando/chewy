0098bb85b8725399c0af1d48a9af8db416686cfc
k <k@ks-macbook-pro.(none)>
2012-11-25 10:21:24 -0600

    adding modfied files to v2 cause i was lazy last time

--- main.py
+++ main.py
@@ -20,68 +20,108 @@ import random
 import os
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
-
 import datetime
 import urllib
 from google.appengine.ext import db
 from google.appengine.api import users
+from google.appengine.api import mail
 
-class Book(db.Model):
-    author = db.StringProperty()
-    title = db.StringProperty()
-    content = db.StringProperty(multiline=True)
+
+# Define Model
+class Translation(db.Model):
+    english = db.StringProperty(multiline=True)
+    wookie = db.StringProperty(multiline=True)
+    url_key = db.IntegerProperty()
+    touched = db.IntegerProperty()
     date = db.DateTimeProperty(auto_now_add=True)
 
-
+# Landing function
 class MainPage(webapp2.RequestHandler):
-	def get(self):
-		templateValues = {
-            'wookie': 'not used',
-		}
+	def get(self,urlKey):
+		
+		if(urlKey==''):
+			templateValues = {'wookie': '...'}
+		else:
+			tid = int(urlKey)
+			translation = Translation.get_by_id(tid)
+			templateValues = {'wookie': translation.wookie}
+			
+
 		path = os.path.join(os.path.dirname(__file__), 'base.html')
 		self.response.out.write(template.render(path, templateValues))
-#		self.response.out.write(template.render('base.html', {}))
-
-
-
-class Guestbook(webapp2.RequestHandler):
+		
+	def post(self,urlKey):
+		userInput = self.request.get('phrase')
+		allTranslations = Translation.all()
+		allTranslations.filter('english =',userInput)
+		
+		
+		if(allTranslations.count() == 1):
+			result = allTranslations.get()
+			newUrlKey = str(result.key().id())
+		if(allTranslations.count() < 1):
+			newUrlKey =  translateToWookie(userInput)
+		if(allTranslations.count() > 1):
+			newUrlKey = "66666"
+			
+		self.redirect("/"+newUrlKey)
+		
+class Email(webapp2.RequestHandler):
     def post(self):
- 		input = self.request.get('phrase')
-		book = Book(title = 'fresh book', author = 'kevo', content = input).put()
-		
-		books = Book.all()
-		books.order("title")
-		bookResults = books.fetch(limit=40)
-		
-		templateValues = {
-            'wookie': translateToWookie(input),
-			'bookResults' : bookResults
-		}
-		
-		
-		
-		path = os.path.join(os.path.dirname(__file__), 'base.html')
-		self.response.out.write(template.render(path, templateValues))
-		
- #     	self.response.out.write(cgi.escape(translateToWookie(self.request.get('phrase'))))
-#       self.response.out.write(cgi.escape(self.request.get('phrase')))
-#        self.response.out.write('</pre></body></html>')
+		kev ="asdf"
+		message = mail.EmailMessage(sender="khabich@gmail.com",
+		                            subject=self.request.get('subject'))
 
-app = webapp2.WSGIApplication([('/', MainPage),
-                              ('/translate', Guestbook)],
+		message.to = "kevin.h@roybiv.net"
+		message.body = ' '+self.request.get('body')
+
+		message.send()
+		self.redirect("/")
+
+		
+class ListAllTranslations(webapp2.RequestHandler):
+	def get(self):
+		self.response.out.write("<style>td{border:solid 1px #B9B9B9;background:#E0E0E0;}</style>")
+		self.response.out.write("<table><tr><td width='30'>ID</td><td>ENGLISH</td><td>WOOKIE</td><td>Date</td></tr>")
+		translations = Translation.all()
+#		translations.order("english")
+		translationSet = translations.fetch(limit=100)
+		for translated in translationSet:
+			self.response.out.write('<tr>')
+			self.response.out.write('<td>'+str(translated.key().id())+'</td>')
+			self.response.out.write('<td>'+translated.english+'</td>')
+			self.response.out.write('<td>'+translated.wookie+'</td>')
+			self.response.out.write('<td>'+str(translated.date)+'</td>')
+			self.response.out.write('</tr>')
+		#	translated.delete()	
+		self.response.out.write('</table>')
+		
+		
+
+app = webapp2.WSGIApplication([('/translations', ListAllTranslations),
+							  ('/email', Email),
+							  ('/(.*)', MainPage)],
                               debug=True)
 
 
 
 def translateToWookie(englishWord):
-	wookieLanguage = ['huurh ', 'uughghhhgh ','uuh ','raaaaaahhgh ','uughguughhhghghghhhgh ', 'huuguughghg ','aarrragghuuhw ','aaaaahnr ']
+	wookieLanguage = ['huurh ', 'uughghhhgh ','uuh ','raaaaaahhgh ','uughguughhhghghghhhgh ', 'huuguughghg ','aarrragghuuhw ','aaaaahnr ','huurh ', 'uughghhhgh ','uuh ','raaaaaahhgh ','uughguughhhghghghhhgh ', 'huuguughghg ','aarrragghuuhw ','aaaaahnr ','huurh ', 'uughghhhgh ','uuh ','raaaaaahhgh ','uughguughhhghghghhhgh ', 'huuguughghg ','aarrragghuuhw ','aaaaahnr ','huurh ', 'uughghhhgh ','uuh ','raaaaaahhgh ','uughguughhhghghghhhgh ', 'huuguughghg ','aarrragghuuhw ','aaaaahnr ']
 	random.shuffle(wookieLanguage)
 	i = englishWord.__len__()/5 + 1
 	translation = ""
 	for num in range(0,i):
 		translation += wookieLanguage[num]
 
-	return translation
+	newTranslation = Translation(english = englishWord, wookie = translation).put()
+
+	# need better way to get this data
+	allTranslations = Translation.all()
+	allTranslations.filter('english =',englishWord)
+	result = allTranslations.get()
+	newUrlKey = str(result.key().id())
+	
+	return newUrlKey
 
 
 
